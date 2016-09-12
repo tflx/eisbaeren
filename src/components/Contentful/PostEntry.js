70b1a6c996112867c602477f9f0f0b15b31f4c86
Torben Frank Larsen <tfl@insilico.dk>
2016-09-12 15:48:26 +0200

    small fixes

--- src/components/Contentful/PostEntry.js
+++ src/components/Contentful/PostEntry.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react';
 import {browserHistory} from 'react-router';
-import {Card, CardHeader, CardText, CardActions} from 'material-ui/Card';
+import {Card, CardHeader, CardActions} from 'material-ui/Card';
 import DateString from 'components/DateString';
 import IconButton from 'components/IconButton';
 import SvgIcon from 'components/SvgIcon/SvgIcon';
@@ -16,7 +16,7 @@ function PostEntry({...props}) {
 
   return (
     <Card>
-      <CardHeader title={title} subtitle={<DateString date={date} parenthesis={false} />}/>
+      <CardHeader title={title} subtitle={<DateString date={date} parenthesis={false} />} />
       <CardActions>
         <IconButton onClick={showPost} icon={<SvgIcon width="24px" svg={arrow} />} />
       </CardActions>
